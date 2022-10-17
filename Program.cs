0620f1f37e2f08431ed57756dad2ab45e723ff57
betof <betof@DESKTOP-5PPJA4A>
2022-10-17 13:15:23 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿class Program
+{
+    static void Main()
+    {
+
+        
+        int[] notas = new int[15];
+
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine("Ingrese nota: ");
+                notas[i] = int.Parse(Console.ReadLine());
+            }
+
+        double prom = 0;
+        double suma = 0;
+            for (int i = 0; i < 15; i++)
+            {
+                suma = suma + notas[i];
+            }
+
+        prom = suma / 15;
+        Console.WriteLine("El promedio obtenido por las notas es de: " + prom);
+
+       
+    }
+}
